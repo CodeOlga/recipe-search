0682413e4fe538136510c1b6d968264cf5a3be52
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2022-11-30 16:17:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import video from './food.mp4';
 
 function App() {
+
+  const MY_ID = '139c6380';
+  const MY_KEY = 'a9c4440dce8fb0b058d4571c6a1f32be';
+
+  const [mySearch, setMySearch] = useState('');
+
+  useEffect(() => {
+    const getRecipe = async() => {
+      const response = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=${MY_ID}&app_key=${MY_KEY}`);
+      const data = await response.json();
+      console.log(data.hits);
+    }
+    getRecipe();
+  }, [])
+
+const myRecipeSearch = (e) => {
+  setMySearch(e.target.value);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <video autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+        </video>
+        <h1>Find a Recipe</h1>
+      </div>
+
+      <div className="container">
+        <form>
+          <input className='search' placeholder='Search...' onChange={myRecipeSearch} value={mySearch}>
+          </input>
+        </form>
+
+        <div className="container">
+          <button>
+            <img src="https://img.icons8.com/fluency/48/000000/fry.png" alt="icon" className='icons'/>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
